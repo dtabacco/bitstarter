8ffbcd4ad0317cdf903661094ed3c31029c8c426
dtabacco <David_tabacco@merck.com>
2013-12-03 19:50:22 +0000

    Updated Web.js with buffer

--- web.js
+++ web.js
@@ -3,7 +3,20 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+//Modified Code for Assignment
+fs.readFile('index.html', function (err, data) {
+  if (err) throw err;
+  //console.log(data);
+  //console.log(data.toString("utf-8"));
+  
+  myData = data.toString("utf-8");
+  response.send(myData);
+});
+
+
+
+ // response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
